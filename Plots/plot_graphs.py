fbcd3f2538f3551b0b9c65cfd1235865dc861385
Egor <149178595+Maklut@users.noreply.github.com>
2026-04-05 23:27:01 +0300

    Update plot_graphs.py

--- Plots/plot_graphs.py
+++ Plots/plot_graphs.py
@@ -1,1 +1,2 @@
-
+<img width="1280" height="768" alt="lin" src="https://github.com/user-attachments/assets/8e5c9939-5a49-4f95-a242-9da85512a27b" />
+<img width="427" height="166" alt="log" src="https://github.com/user-attachments/assets/63affb21-be20-44bb-8243-fa10e8f6a860" />
