4ec670d04b0ca6e1a3ab97ddc8cf587bab5e7e09
Egor <149178595+Maklut@users.noreply.github.com>
2026-04-12 23:32:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -13,25 +13,9 @@ int countPairs1(int *arr, int len, int value) {
 }
 
 int countPairs2(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len; i++) {
-    for (int j = i + 1; j < len; j++) {
-      if (arr[i] + arr[j] == value) {
-        count++;
-      }
-    }
-  }
-  return count;
+  return countPairs1(arr, len, value);
 }
 
 int countPairs3(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len; i++) {
-    for (int j = i + 1; j < len; j++) {
-      if (arr[i] + arr[j] == value) {
-        count++;
-      }
-    }
-  }
-  return count;
+  return countPairs1(arr, len, value);
 }
