02ef8d3e14dac2bb7d1c05f9f1fc7928123a54ea
Egor <149178595+Maklut@users.noreply.github.com>
2026-04-12 23:20:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,25 +19,9 @@ int countPairs2(int *arr, int len, int value) {
   while (left < right) {
     int sum = arr[left] + arr[right];
     if (sum == value) {
-      if (arr[left] == arr[right]) {
-        int n = right - left + 1;
-        count += n * (n - 1) / 2;
-        break;
-      } else {
-        int left_val = arr[left];
-        int right_val = arr[right];
-        int left_count = 0;
-        int right_count = 0;
-        while (left < len && arr[left] == left_val) {
-          left_count++;
-          left++;
-        }
-        while (right >= 0 && arr[right] == right_val) {
-          right_count++;
-          right--;
-        }
-        count += left_count * right_count;
-      }
+      count++;
+      left++;
+      right--;
     } else if (sum < value) {
       left++;
     } else {
@@ -57,11 +41,6 @@ int countPairs3(int *arr, int len, int value) {
       int mid = (left + right) / 2;
       if (arr[mid] == target) {
         count++;
-        int temp = mid + 1;
-        while (temp < len && arr[temp] == target) {
-          count++;
-          temp++;
-        }
         break;
       } else if (arr[mid] < target) {
         left = mid + 1;
